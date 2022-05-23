7c06010ae8ac516b3cc3b4bf93107008cd51fc19
taufiq <taufiq.goe@gmail.com>
2022-05-23 19:47:08 +0800

    List Product by @name @page @size at /api/p

--- src/main/kotlin/c22/ps167/backendrestfulapi/data/repository/ProductRepository.kt
+++ src/main/kotlin/c22/ps167/backendrestfulapi/data/repository/ProductRepository.kt
@@ -1,6 +1,15 @@
 package c22.ps167.backendrestfulapi.data.repository
 
 import c22.ps167.backendrestfulapi.data.entity.Product
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface ProductRepository : JpaRepository<Product, String>
+interface ProductRepository : JpaRepository<Product, String> {
+
+    @Query("SELECT u FROM Product u WHERE u.name LIKE %:name%")
+    fun findAllByProductName(@Param(value = "name") name: String, pageable: Pageable): Page<Product>
+
+}
